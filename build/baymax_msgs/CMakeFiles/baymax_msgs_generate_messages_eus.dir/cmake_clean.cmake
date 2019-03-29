4ecb18d061fa1a035aa5ca961018ec7f786a69a9
khaled osman <khaledosman737@gmail.com>
2019-03-29 14:00:41 +0200

    Initial commit

---
+++ build/baymax_msgs/CMakeFiles/baymax_msgs_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/baymax_msgs_generate_messages_eus"
+  "/home/khaled/baymax_ws2/devel/share/roseus/ros/baymax_msgs/msg/Velocities.l"
+  "/home/khaled/baymax_ws2/devel/share/roseus/ros/baymax_msgs/msg/PID.l"
+  "/home/khaled/baymax_ws2/devel/share/roseus/ros/baymax_msgs/msg/Imu.l"
+  "/home/khaled/baymax_ws2/devel/share/roseus/ros/baymax_msgs/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/baymax_msgs_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ devel/share/gennodejs/ros/baymax_msgs/msg/_index.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+let PID = require('./PID.js');
+let Imu = require('./Imu.js');
+let Velocities = require('./Velocities.js');
+
+module.exports = {
+  PID: PID,
+  Imu: Imu,
+  Velocities: Velocities,
+};
